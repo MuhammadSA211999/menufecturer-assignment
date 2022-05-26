efc2134ce0badad8e88632e7bc34b79de96f12c9
Muhammad SA <MuhammadSA211999>
2022-05-26 15:30:00 +0600

    single by id

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
         });
-
+        // load a single parts 
         app.get('/parts/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
